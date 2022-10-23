ed105af4665be329cd43ecb451efe968ac34b843
sozoro <46480375+sozoro@users.noreply.github.com>
2022-10-23 11:55:54 +0900

    appendEmptyPage

--- append.sh
+++ append.sh
@@ -8,8 +8,8 @@ pics=`ls -1 | grep ".*\.jpg"`
 
 echo $pics | awk \
   -v cmd="convert +append " \
-  '{ for (i=1; i<=(NF/2); i++) { \
+  '{ for (i=1; i<=(NF/2); i++) {
        n=int(log(NF/2)/log(10)+0.000000000000001)+1
-       printf cmd$(i*2)" "$(i*2-1)" appended_%0"n"d.jpg\n", i; \
+       printf cmd$(i*2)" "$(i*2-1)" appended_%0"n"d.jpg\n", i;
      } \
    }' | bash -x
---
+++ appendEmptyPage.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env nix-shell
+#!nix-shell -i bash -p imagemagick
+
+# set -x
+
+opts=`getopt --unquoted --options "o:lr" -- $@`
+
+args=""
+output=""
+gravity="West"
+
+eval `echo $opts | awk \
+  '{ for(i=1; i<=NF; i++) {
+       switch($i) {
+         case "-o":
+           i++
+           printf "output=\""$i"\"; "
+           break
+         case "-l":
+           printf "gravity=\"East\"; "
+           break
+         case "-r":
+           printf "gravity=\"West\"; "
+           break
+         case "--":
+           i++
+           for(; i<=NF; i++) {
+             printf "args=$args\" "$i"\"; "
+           }
+           break
+       }
+     }
+  }'`
+
+input=`echo $args | awk '{ print $1 }'`
+
+if [ -z $input ]; then
+  echo "no input file" >&2
+  exit 1
+fi
+
+if [ -z $output ]; then
+  output=`echo $input | sed -r 's/^(.*)(\.[^\.]*)$/\1_aep\2/'`
+fi
+
+wxh=`identify $input | awk '{ print $3 }'`
+width=`echo $wxh | sed 's/x.*$//'`
+height=`echo $wxh | sed 's/^.*x//'`
+
+# convert -list gravity
+
+convert $input -gravity $gravity -extent $(($width*2))x${height} $output
